abe79bdf90652c587938526f0b4ab59f96920084
Joel Aisien <joelaisien8@gmail.com>
2022-09-01 01:40:18 +0100

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,12 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    sum_scores = sum(map(lambda x: x[0] * x[1], my_list))
-    sum_weights = sum(map(lambda x: x[1], my_list))
-    return sum_scores/
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
